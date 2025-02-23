f96745c3f6ebc7d1e4e099a91c6bf5dd1f2f0b12
SspidersS <99414766+SspidersS@users.noreply.github.com>
2025-02-23 21:51:40 +0100

    в

--- java/org/example/exceptions/TicketValidationException.java
+++
@@ -1,7 +0,0 @@
-package org.example.exceptions;
-
-public class TicketValidationException extends RuntimeException {
-    public TicketValidationException(String message) {
-        super(message);
-    }
-}
